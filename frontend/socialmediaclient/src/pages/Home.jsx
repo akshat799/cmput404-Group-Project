7b55bf1fdc6b84e8bbb17c72578216f425eb851c
Shubham <86523618+shubham-sb1@users.noreply.github.com>
2022-10-28 05:54:32 -0600

    Add files via upload

---
+++ frontend/socialmediaclient/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Feed from "../components/Feed"
+import Navbar from '../components/Navbar';
+import "./home.css"
+
+
+export default function Home() {
+  return (
+ 
+    <div className="home_container">
+        <Navbar/>
+        <Feed />
+        
+    </div>
+    
+  );
+}
