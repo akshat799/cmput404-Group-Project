c741e70ab077240d5be297b0d2d49bf3974ad59d
raghavchandak <raghavchandak25@gmail.com>
2022-10-28 15:19:53 -0600

    Axios setup done

--- frontend/socialmediaclient/src/components/Navbar.jsx
+++ frontend/socialmediaclient/src/components/Navbar.jsx
@@ -6,13 +6,11 @@ import InboxIcon from '@mui/icons-material/Inbox';
 export default function Navbar() {
   return (
     <div className="navbar">
-      
-        <span class = "name">Social Media App</span>
-        <span className="inbox"><button className="buttoni"><InboxIcon></InboxIcon>Inbox</button></span>
-        <span className="profile"><button className="buttonp"><AccountCircleIcon></AccountCircleIcon>Profile</button></span>
+        <p style={{marginLeft: "0.5rem"}} className="name">Social Connection</p>
+        <div class = "options">
+          <span className="inbox"><button className="buttoni"><InboxIcon></InboxIcon>Inbox</button></span>
+          <span className="profile"><button className="buttonp"><AccountCircleIcon></AccountCircleIcon>Profile</button></span>
         </div>
-   
-
-   
+    </div>
   );
 }
