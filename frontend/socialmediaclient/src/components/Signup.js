d6720a990c92bf2a2e8391410fb047fe78667dd2
raghavchandak <raghavchandak25@gmail.com>
2022-10-28 17:02:31 -0600

    Added Routers and register api

--- frontend/socialmediaclient/src/components/Signup.js
+++ frontend/socialmediaclient/src/components/Signup.js
@@ -1,34 +1,118 @@
-import { Avatar, Button, Grid, Link, TextField, Typography } from '@mui/material';
-import {Paper} from "@mui/material";
+import { Avatar, Button, Grid, TextField, Typography } from "@mui/material";
+import { Paper } from "@mui/material";
 import React, { useState } from "react";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { registerCall } from "../api/apiCall";
 
-const Signup=() => {
+const Signup = () => {
+  const paperStyle = {
+    padding: 20,
+    height: "70vh",
+    width: 300,
+    margin: "0px auto",
+  };
+  const avatarStyle = { backgroundColor: "#9494de" };
+  const formFieldStyle = { margin: "10px 0px 0px 0px" };
+  const headerStyle = { margin: 0 };
 
-    const paperStyle={padding: 20, height: '70vh', width: 300, margin:"0px auto"}
-    const avatarStyle={backgroundColor: '#9494de'}
-    const formFieldStyle ={margin: '10px 0px 0px 0px'}
-    const headerStyle = {margin:0}
+  const initialState = {
+    username: "",
+    firstname: "",
+    lastname: "",
+    password: "",
+    github: "",
+  };
 
-    return (
-        <Grid>
-             <Paper style={paperStyle}>
-                <Grid align='center'> 
-                    <Avatar style= {avatarStyle} ><AddCircleOutlineIcon/></Avatar>
-                    <h2 style={headerStyle}> Sign Up</h2>
-                    <Typography variant='caption'> Complete this form to create an account </Typography>
-                </Grid>
-                <form>
-                    <TextField variant="standard" label='Username' placeholder="Enter Username" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='First Name' placeholder="Enter First Name" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='Last Name' placeholder="Enter Last Name" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='Password' placeholder="Enter Password" style={formFieldStyle} fullWidth required type='password'/>
-                    <TextField variant="standard" label='Confirm Password' placeholder="Confirm Password" style={formFieldStyle} fullWidth required type='password'/>
-                    <Button style={{margin: '25px 0px 10px 0px'}} variant='contained' type='submit' color='primary' fullWidth> Sign Up</Button>                
-                </form>
-            </Paper>
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <Grid>
+      <Paper style={paperStyle}>
+        <Grid align="center">
+          <Avatar style={avatarStyle}>
+            <AddCircleOutlineIcon />
+          </Avatar>
+          <h2 style={headerStyle}> Sign Up</h2>
+          <Typography variant="caption">
+            {" "}
+            Complete this form to create an account{" "}
+          </Typography>
         </Grid>
-    )
-} 
+        <form>
+          <TextField
+            variant="standard"
+            label="Username"
+            placeholder="Enter Username"
+            style={formFieldStyle}
+            fullWidth
+            required
+            handleChange={handleChange}
+          />
+          <TextField
+            variant="standard"
+            label="First Name"
+            placeholder="Enter First Name"
+            style={formFieldStyle}
+            fullWidth
+            required
+            handleChange={handleChange}
+          />
+          <TextField
+            variant="standard"
+            label="Last Name"
+            placeholder="Enter Last Name"
+            style={formFieldStyle}
+            fullWidth
+            required
+            handleChange={handleChange}
+          />
+          <TextField
+            variant="standard"
+            label="Password"
+            placeholder="Enter Password"
+            style={formFieldStyle}
+            fullWidth
+            required
+            type="password"
+            handleChange={handleChange}
+          />
+          <TextField
+            variant="standard"
+            label="Confirm Password"
+            placeholder="Confirm Password"
+            style={formFieldStyle}
+            fullWidth
+            required
+            type="password"
+            handleChange={handleChange}
+          />
+          <TextField
+            variant="standard"
+            label="GitHub Name"
+            placeholder="GitHub Name"
+            style={formFieldStyle}
+            fullWidth
+            handleChange={handleChange}
+          />
+          <Button
+            style={{ margin: "25px 0px 10px 0px" }}
+            variant="contained"
+            type="submit"
+            color="primary"
+            fullWidth
+            onClick={registerCall(formData)}
+          >
+            {" "}
+            Sign Up
+          </Button>
+        </form>
+      </Paper>
+    </Grid>
+  );
+};
 
-export default Signup
+export default Signup;
