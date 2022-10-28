818a7756c77dea232dded9954541734c4d2d97d6
raghavchandak <raghavchandak25@gmail.com>
2022-10-28 17:02:31 -0600

    Added Routers and register api

--- frontend/socialmediaclient/src/App.js
+++ frontend/socialmediaclient/src/App.js
@@ -1,19 +1,18 @@
-import './App.css';
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
-import LoginSignUpContainer from './containers/LoginSignUpContainer';
-import Profile from './pages/profile/Profile';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginSignUpContainer from "./containers/LoginSignUpContainer";
+import Profile from "./pages/profile/Profile";
+import Home from "./pages/Home.jsx";
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Profile />
-        {/* <h1>Homepage</h1> */}
-      {/* <Routes>
-          {/* <Route path= "/" element={<LoginSignUpContainer/>} /> */}
-          
-      {/* </Routes> */}
-        
+        <Routes>
+          <Route path="/" element={<LoginSignUpContainer />} />
+          <Route exact path="/home" element={<Home />} />
+          <Route exact path="/profile" element={<Profile />} />
+        </Routes>
       </div>
     </Router>
   );
