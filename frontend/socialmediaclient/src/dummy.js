ed6a66f1ad5ffefac240dbf8c65b89cd87890f18
shubham <shubhambajoria01@gmail.com>
2022-10-28 07:26:36 -0600

    feed

---
+++ frontend/socialmediaclient/src/dummy.js
@@ -0,0 +1,47 @@
+export const Users = [
+    {
+      id:1,
+      profilePicture: "images/1.png",
+      username: "Safak Kocaoglu",
+    },
+    {
+      id:2,
+      profilePicture: "images/2.png",
+      username: "Janell Shrum",
+    },
+    {
+      id:3,
+      profilePicture: "images/3.png",
+      username: "Alex Durden",
+    },
+ 
+  ];
+  
+  export const Posts = [
+    {
+      id: 1,
+      desc: "Love For All, Hatred For None.",
+      photo: "images/1.png",
+      date: "5 mins ago",
+      userId: 1,
+      like: 32,
+      comment: 9,
+    },
+    {
+      id: 2,
+      photo: "images/2.png",
+      date: "15 mins ago",
+      userId: 2,
+      like: 2,
+      comment: 1,
+    },
+    {
+      id: 3,
+      desc: "Every moment is a fresh beginning.",
+      photo: "images/3.png",
+      date: "1 hour ago",
+      userId: 3,
+      like: 61,
+      comment: 2,
+    },
+  ];
