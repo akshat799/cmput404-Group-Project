70b9850972f7d556258ba502f4a2674848fbf7a3
jwadhwa213 <jwadhwa@ualberta.ca>
2022-10-28 15:16:22 -0600

    Merged

--- frontend/socialmediaclient/src/App.js
+++ frontend/socialmediaclient/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom';
 import LoginSignUpContainer from './containers/LoginSignUpContainer';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Homepage</h1>
-      <Routes>
-          <Route path= "/" element={<LoginSignUpContainer/>} />
-      </Routes>
+        <Profile />
+        {/* <h1>Homepage</h1> */}
+      {/* <Routes>
+          {/* <Route path= "/" element={<LoginSignUpContainer/>} /> */}
+          
+      {/* </Routes> */}
         
       </div>
     </Router>
