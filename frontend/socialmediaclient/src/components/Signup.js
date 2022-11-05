5ac6b2197f840419e5d8f58ba568e19c5a8e46b8
jwadhwa213 <jwadhwa@ualberta.ca>
2022-11-05 14:42:28 -0600

    Store configured

--- frontend/socialmediaclient/src/components/Signup.js
+++ frontend/socialmediaclient/src/components/Signup.js
@@ -2,13 +2,33 @@ import { Avatar, Button, Grid, Link, TextField, Typography } from '@mui/material
 import {Paper} from "@mui/material";
 import React, { useState } from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { useSelector } from "react-redux";
 
 const Signup=() => {
+
+    const { isSignedIn } = useSelector((state) => state.auth.isSignedIn);
+    const dispatch = useDispatch();
+
+    const [username, setUsername] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirmPassword, setConfirmPassword] = useState("")
+
 
     const paperStyle={padding: 20, height: '70vh', width: 300, margin:"0px auto"}
     const avatarStyle={backgroundColor: '#9494de'}
     const formFieldStyle ={margin: '10px 0px 0px 0px'}
     const headerStyle = {margin:0}
+    const handleSubmit = () => {
+        if(password === confirmPassword){
+            
+        }
+        else{
+            
+        }
+
+    }
 
     return (
         <Grid>
@@ -19,12 +39,68 @@ const Signup=() => {
                     <Typography variant='caption'> Complete this form to create an account </Typography>
                 </Grid>
                 <form>
-                    <TextField variant="standard" label='Username' placeholder="Enter Username" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='First Name' placeholder="Enter First Name" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='Last Name' placeholder="Enter Last Name" style={formFieldStyle} fullWidth required/>
-                    <TextField variant="standard" label='Password' placeholder="Enter Password" style={formFieldStyle} fullWidth required type='password'/>
-                    <TextField variant="standard" label='Confirm Password' placeholder="Confirm Password" style={formFieldStyle} fullWidth required type='password'/>
-                    <Button style={{margin: '25px 0px 10px 0px'}} variant='contained' type='submit' color='primary' fullWidth> Sign Up</Button>                
+                    <TextField 
+                        className='username' 
+                        value = {username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        variant="standard" 
+                        label='Username' 
+                        placeholder="Enter Username" 
+                        style={formFieldStyle} 
+                        fullWidth 
+                        required/>
+                    <TextField 
+                        className='firstName'
+                        value = {firstName} 
+                        onChange={(e) => setFirstName(e.target.value)}
+                        variant="standard" 
+                        label='First Name' 
+                        placeholder="Enter First Name" 
+                        style={formFieldStyle} 
+                        fullWidth 
+                        required/>
+                    <TextField 
+                        className='lastName'
+                        value = {lastName}
+                        onChange={(e) => setLastName(e.target.value)} 
+                        variant="standard" 
+                        label='Last Name' 
+                        placeholder="Enter Last Name" 
+                        style={formFieldStyle} 
+                        fullWidth 
+                        required/>
+                    <TextField 
+                        className='password' 
+                        value = {password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        variant="standard" 
+                        label='Password' 
+                        placeholder="Enter Password" 
+                        style={formFieldStyle} 
+                        fullWidth 
+                        required 
+                        type='password'/>
+                    <TextField 
+                        className='confirmPassword'
+                        value = {confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)} 
+                        variant="standard" 
+                        label='Confirm Password' 
+                        placeholder="Confirm Password" 
+                        style={formFieldStyle} 
+                        fullWidth 
+                        required 
+                        type='password'/>
+                    <Button 
+                        className='submit' 
+                        style={{margin: '25px 0px 10px 0px'}} 
+                        variant='contained' 
+                        type='submit' 
+                        color='primary' 
+                        fullWidth
+                        onClick={handleSubmit}
+                        > 
+                        Sign Up</Button>                
                 </form>
             </Paper>
         </Grid>
