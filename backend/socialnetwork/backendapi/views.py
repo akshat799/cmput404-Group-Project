b09a75682a70ae2aad70f8ded7142b995527ff31
raghavchandak <raghavchandak25@gmail.com>
2022-11-23 05:34:19 -0700

    Added missing pieces

--- backend/socialnetwork/backendapi/views.py
+++ backend/socialnetwork/backendapi/views.py
@@ -1,9 +1,6 @@
-from email import message
-from logging import raiseExceptions
-import profile
 from pyexpat.errors import messages
-from django.shortcuts import render
-from requests import request
+from pyexpat import model
+from django.shortcuts import render, get_object_or_404
 from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.viewsets import ModelViewSet
@@ -18,6 +15,8 @@ from . import models
 import json
 import uuid
 
+
+url = "http://127.0.0.1:8000/"
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
@@ -140,6 +139,7 @@ class RegistrationViewSet(ModelViewSet, TokenObtainPairView):
             "refresh": res["refresh"],
             "token": res["access"]
         }, status=status.HTTP_201_CREATED)
+
 
 @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 @permission_classes([IsAuthenticated])
@@ -372,3 +372,157 @@ def LikedViewSet(requests,author_id):
     except Exception as e:
         data = {'error': str(e)}
         return Response(data,status=status.HTTP_400_BAD_REQUEST)
+        
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def CommentViewSet(request, author_id, post_id):
+
+    paginator = PageNumberPagination()
+    paginator.page_size = 10
+    page_query_param = 'page'
+    page_size_query_param = 'size'
+
+    if(request.method == 'GET'):
+        try:
+            comments = models.CommentModel.objects.filter(post = post_id)
+            result_page = paginator.paginate_queryset(comments, request)
+            serializer = serializers.CommentSerializer(result_page, many = True)
+            # return Response(serializer.data, status = status.HTTP_200_OK)
+            return paginator.get_paginated_response(serializer.data)
+        except Exception as e:
+            data = {'error' : str(e)}
+            return Response(data, status = status.HTTP_400_BAD_REQUEST)
+
+    if(request.method == 'POST'):
+        try:
+            data = request.data
+            author = get_object_or_404(models.Users,id = author_id)
+            data.update({"post" : post_id, "author" : author_id})
+            serializer = serializers.CommentSerializer(data = data)
+            serializer.is_valid(raise_exception = True)
+            serializer.save()
+            authorData = {
+                "type": author.type,
+                "id": author.id,
+                "host": author.host,
+                "displayName": author.displayName,
+                "url": author.url,
+                "github": f'http://github.com/{author.githubName}',
+                "profileImage": author.profileImage
+            }
+
+            responseData = {
+                "type" : "comment",
+                "author" : authorData,
+                "comment" : serializer.data["comment"],
+                "contentType" : serializer.data["contentType"],
+                "published" : serializer.data["published"],
+                "id" : serializer.data["id"],
+            }
+            return Response(responseData, status = status.HTTP_200_OK)
+        except Exception as e:
+            data = {'error' : str(e)}
+            return Response(data, status = status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+@permission_classes([IsAuthenticated])
+def FollowerViewSet(request, author_id, foreign_author_id = None):
+    
+    if(foreign_author_id == None):
+        if(request.method == 'GET'):
+            try:
+                followers = models.FollowerModel.objects.filter(followedAuthor = author_id)
+                serializer = serializers.FollowerSerializer(followers, many = True)
+                print('PRINTING SERIALZER DATA : ',serializer.data)
+                authorList = []
+                for follower in serializer.data:
+                    print(follower)
+                    author = get_object_or_404(models.Users, id = follower.follower_id)
+                    print(author)
+                    authorData = {
+                        "type": author.type,
+                        "id": author.id,
+                        "host": author.host,
+                        "displayName": author.displayName,
+                        "url": author.url,
+                        "github": f'http://github.com/{author.githubName}',
+                        "profileImage": author.profileImage
+                    }
+                    authorList.append(authorData)
+
+                returnVal = {
+                    "type" : "followers",
+                    "items" : authorList
+                }
+
+                return Response(returnVal, status = status.HTTP_200_OK)
+            except Exception as e:
+                data = {'error' : str(e)}
+                return Response(data, status = status.HTTP_400_BAD_REQUEST)
+
+    else:
+        if(request.method == 'GET'):
+            try:
+                if models.FollowerModel.objects.filter(follower = foreign_author_id, followedAuthor = author_id).exists():
+                    data = "IS A FOLLOWER"
+                else:
+                    data = "IS NOT A FOLLOWER"
+                return Response(data, status = status.HTTP_200_OK)
+            except Exception as e:
+                data = {'error' : str(e)}
+                return Response(data, status = status.HTTP_400_BAD_REQUEST)
+
+        elif(request.method == 'PUT'):
+            try:
+                data = request.body
+                jsonResponse = json.loads(data.decode('utf-8'))
+                author = models.Users.objects.get(id = foreign_author_id)
+                jsonResponse.update({"followedAuthor" : author_id, "follower" : foreign_author_id})
+                serializer = serializers.FollowerSerializer(data = jsonResponse)
+                serializer.is_valid(raise_exception = True)
+                serializer.save()
+                follower = get_object_or_404(models.Users, id = foreign_author_id)
+                followedAuthor = models.Users.objects.get(id = author_id)
+
+                followerData = {
+                    "type": follower.type,
+                    "id": follower.id,
+                    "host": follower.host,
+                    "displayName": follower.displayName,
+                    "url": follower.url,
+                    "github": f'http://github.com/{follower.githubName}',
+                    "profileImage": follower.profileImage
+                }
+
+                followedAuthorData = {
+                    "type": followedAuthor.type,
+                    "id": followedAuthor.id,
+                    "host": followedAuthor.host,
+                    "displayName": followedAuthor.displayName,
+                    "url": followedAuthor.url,
+                    "github": f'http://github.com/{followedAuthor.githubName}',
+                    "profileImage": followedAuthor.profileImage
+                }
+
+                returnVal = {
+                    "type" : "follow",
+                    "follower" : followerData,
+                    "followed author" : followedAuthorData
+                }
+                return Response(returnVal, status = status.HTTP_201_CREATED)
+            except Exception as e:
+                data = {'error' : str(e)}
+                return Response(data, status = status.HTTP_400_BAD_REQUEST)
+
+        elif(request.method == 'DELETE'):
+            
+            try:
+                follow = get_object_or_404(models.FollowerModel, follower = foreign_author_id, followedAuthor = author_id)
+                print('FOLLOW OBJECT:', follow)
+                serializer = serializers.FollowerSerializer(follow)
+                follow.delete()
+                data = {"message":"Deletion successful"}
+                return Response(data, status = status.HTTP_204_NO_CONTENT)
+            except Exception as e:
+                data = {'error' : str(e)}
+                return Response(data, status = status.HTTP_400_BAD_REQUEST)
