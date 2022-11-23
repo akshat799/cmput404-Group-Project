1554403fd6d8258ad3d6edea2bebe8dad095951b
akshat799 <gulati.akshat799@gmail.com>
2022-11-23 03:33:00 -0700

    Frontend error resolved

--- frontend/socialmediaclient/src/api/apiCall.js
+++ frontend/socialmediaclient/src/api/apiCall.js
@@ -2,9 +2,9 @@ import * as api from "./index"
 
 export const registerCall = async(data) => {
     try{
-        response = await api.register(data);
-        localStorage.setItem({'token' : data.access})
-        return data;
+        let response = await api.register(data);
+        localStorage.setItem({'token' : response.access})
+        return response;
     }
     catch(e){
         console.log(e);
