c571a5779d28f6b3689edb59489887a14c275950
Jianbang Chen <77318357+Jasonchen-66@users.noreply.github.com>
2022-11-22 20:22:51 -0700

    backend

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = '7luhf_0)i#wc*%1(p=0$3u!bd8qc+$!$yzw%_c9f*=2(mw%shi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -143,3 +145,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
