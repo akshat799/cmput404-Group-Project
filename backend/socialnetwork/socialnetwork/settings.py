36e1024cbeb51228ff12e0fac979843785021314
Jasonchen-66 <jianbang@ualberta.ca>
2022-11-28 03:25:57 -0700

    push

--- backend/socialnetwork/socialnetwork/settings.py
+++ backend/socialnetwork/socialnetwork/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
