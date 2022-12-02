acea0130c68c854c68fdd67ef73651a53bf8d768
shubham <shubhambajoria01@gmail.com>
2022-12-02 12:31:29 -0700

    search box and notification for inbox done

--- frontend/socialmediaclient/src/pages/Home.jsx
+++ frontend/socialmediaclient/src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import Feed from "../components/Feed"
+import React from 'react';
+import Feed from "../components/Feed";
 import Navbar from '../components/Navbar';
-import "./home.css"
+import "./home.css";
 
 export default function Home() {
+  React.useEffect(() => {
+    document.body.style.backgroundColor = "white";
+  }, []);
   return (
     <div className="home_container">
-        <Navbar/>
-        <Feed />
+      <Navbar />
+      <Feed />
     </div>
   );
 }
