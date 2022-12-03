1ac9bf1909e1bb48fc3b343b1eaddb86b527e12d
shubham <shubhambajoria01@gmail.com>
2022-12-03 11:21:16 -0700

    edit post, comment like and sign up modal changes

--- frontend/socialmediaclient/src/components/PostOption.jsx
+++ frontend/socialmediaclient/src/components/PostOption.jsx
@@ -169,7 +169,7 @@ export default function PostOption() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 300,
+    width: 400,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
