e61965b2d3c6493f4aa8558affabfca14f344902
raghavchandak <raghavchandak25@gmail.com>
2022-12-05 17:48:35 -0700

    Merged

--- frontend/socialmediaclient/src/pages/profile/Profile.jsx
+++ frontend/socialmediaclient/src/pages/profile/Profile.jsx
@@ -1,35 +1,43 @@
 import Fab from "@material-ui/core/Fab";
 import Modal from "@material-ui/core/Modal";
 import AccountCircle from "@mui/icons-material/AccountCircle";
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
-import EditIcon from '@mui/icons-material/Edit';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import EditIcon from "@mui/icons-material/Edit";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
 import InputAdornment from "@mui/material/InputAdornment";
 import TextField from "@mui/material/TextField";
-import React from 'react';
+import React from "react";
 import { useSelector } from "react-redux";
 import AuthorPosts from "../../components/AuthorPosts";
 import Navbar from "../../components/Navbar";
 import "./Profile.css";
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 500,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
 };
 
-
-
+const formStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 300,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Profile() {
-
   const state = useSelector((state) => state);
   const [req, setReq] = React.useState(false);
   const [imageOpen, setImageOpen] = React.useState(false);
@@ -38,6 +46,33 @@ export default function Profile() {
   const ProfilePhoto = state.auth.author.profileImage;
   const Username = state.auth.author.displayName;
 
+  const handleImageOpen = () => setImageOpen(true);
+  const handleFormOpen = () => setFormOpen(true);
+  const handleImageClose = () => setImageOpen(false);
+  const handleFormClose = () => setFormOpen(false);
+  const [selectedFile, setSelectedFile] = React.useState();
+  const [preview, setPreview] = React.useState();
+  React.useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined);
+      return;
+    }
+
+    const objectUrl = URL.createObjectURL(selectedFile);
+    setPreview(objectUrl);
+
+    // free memory when ever this component is unmounted
+    return () => URL.revokeObjectURL(objectUrl);
+  }, [selectedFile]);
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setSelectedFile(undefined);
+      return;
+    }
+
+    // I've kept this example simple by using the first image instead of multiple
+    setSelectedFile(e.target.files[0]);
+  };
 
   return (
     <>
@@ -48,7 +83,12 @@ export default function Profile() {
           src={process.env.PUBLIC_URL + "/images/CoverPhoto.jpg"}
           alt=""
         />
-        <img className="profilePhoto" onClick={handleImageOpen} src={ProfilePhoto} alt="" />
+        <img
+          className="profilePhoto"
+          onClick={handleImageOpen}
+          src={ProfilePhoto}
+          alt=""
+        />
         {/* upload image modal */}
         <Modal
           open={imageOpen}
@@ -63,38 +103,56 @@ export default function Profile() {
               type="file"
               onChange={onSelectFile}
               hidden
-
             />
             <label htmlFor="contained-button-file">
-              <Fab component="span" style={{
-                position: 'absolute',
-                right: '120px',
-                bottom: '80px'
-              }} >
+              <Fab
+                component="span"
+                style={{
+                  position: "absolute",
+                  right: "120px",
+                  bottom: "80px",
+                }}
+              >
                 <AddPhotoAlternateIcon />
               </Fab>
             </label>
             {/* <input type='file' accept="image/*" onChange={onSelectFile} /> */}
-            <Box textAlign='center' style={{ marginTop: 10 }}>
-              {selectedFile ? <img alt="nothing" style={{ height: '300px', width: '300px', borderRadius: '50%' }} src={preview} /> : <img alt="nothing" style={{ height: '300px', width: '300px', borderRadius: '50%' }} src={ProfilePhoto} />}
+            <Box textAlign="center" style={{ marginTop: 10 }}>
+              {selectedFile ? (
+                <img
+                  alt="nothing"
+                  style={{
+                    height: "300px",
+                    width: "300px",
+                    borderRadius: "50%",
+                  }}
+                  src={preview}
+                />
+              ) : (
+                <img
+                  alt="nothing"
+                  style={{
+                    height: "300px",
+                    width: "300px",
+                    borderRadius: "50%",
+                  }}
+                  src={ProfilePhoto}
+                />
+              )}
             </Box>
 
-            <Box textAlign='center' style={{ marginTop: 10 }}>
-              <Button variant='contained'>
-                Post
-              </Button>
+            <Box textAlign="center" style={{ marginTop: 10 }}>
+              <Button variant="contained">Post</Button>
             </Box>
           </Box>
         </Modal>
         {/* upload image modal */}
       </div>
       <div className="profileInfo">
-        <div className='nameAdd'>
+        <div className="nameAdd">
           <h4 className="profileName">{Username} </h4>
         </div>
-        <div style={{ display: 'flex' }}>
-
-
+        <div style={{ display: "flex" }}>
           <span className="profileDescription">Welcome to my profile! </span>
           <EditIcon sx={{ marginLeft: 2 }} onClick={handleFormOpen} />
           <Modal
@@ -104,20 +162,21 @@ export default function Profile() {
             aria-describedby="modal-modal-description"
           >
             <Box sx={formStyle}>
-              <h4 style={{ color: 'black', textAlign: 'center' }}>Update your profile</h4>
-              <Box textAlign='center' style={{ marginTop: 10 }}>
+              <h4 style={{ color: "black", textAlign: "center" }}>
+                Update your profile
+              </h4>
+              <Box textAlign="center" style={{ marginTop: 10 }}>
                 <TextField
                   id="input-with-icon-textfield"
                   label="Username"
                   placeholder="testuser1"
                   style={{ marginTop: 10 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
@@ -127,12 +186,11 @@ export default function Profile() {
                   placeholder="test name"
                   style={{ marginTop: 20 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
@@ -143,12 +201,11 @@ export default function Profile() {
                   placeholder="test@gmail.com"
                   style={{ marginTop: 20 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
@@ -158,17 +215,18 @@ export default function Profile() {
                   placeholder="usergithub"
                   style={{ marginTop: 20, marginBottom: 20 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
 
-                <h4 style={{ color: 'black', textAlign: 'center' }}>Update your Password</h4>
+                <h4 style={{ color: "black", textAlign: "center" }}>
+                  Update your Password
+                </h4>
 
                 <TextField
                   id="input-with-icon-textfield"
@@ -177,12 +235,11 @@ export default function Profile() {
                   type="password"
                   style={{ marginTop: 20 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
@@ -193,16 +250,14 @@ export default function Profile() {
                   type="password"
                   style={{ marginTop: 20 }}
                   InputProps={{
-
                     startAdornment: (
                       <InputAdornment position="start">
                         <AccountCircle />
                       </InputAdornment>
-                    )
+                    ),
                   }}
                   variant="standard"
                 />
-
               </Box>
             </Box>
           </Modal>
